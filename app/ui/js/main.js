1025a1d741d7c0bc09e50f1e9c14350420771dde
Micah Y. Bala <micahkulien@gmail.com>
2019-02-10 18:04:36 +0100

    [Feature #163843732] Added javascript

---
+++ app/ui/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Select DOM elements
+let signupTab = document.querySelector('.create');
+let loginTab = document.querySelector('.login');
+let confirmInput = document.querySelector('.hide');
+let btnSubmit = document.querySelector('.btnSubmit');
+let passwordReset = document.querySelector('.pw-reset');
