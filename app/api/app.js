434370c1ca27592595974ea7c05d29973de12a37
Micah Y. Bala <micahkulien@gmail.com>
2019-02-14 15:04:33 +0100

    [Feature #163844411] Installed eslint

--- app/api/app.js
+++ app/api/app.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => res.send("The API is working GREAT!!!"));
+app.get('/', (req, res) => res.send('The API is working GREAT!!!'));
 
 const PORT = 3000;
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
