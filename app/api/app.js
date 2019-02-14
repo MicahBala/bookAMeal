e3441549d8c71ac7cd88b42f0e038065efecd6d1
Micah Y. Bala <micahkulien@gmail.com>
2019-02-14 11:38:09 +0100

    [Feature #163844411] Installed dependencies, babel, nodemon

---
+++ app/api/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("The API is working GREAT!!!"));
+
+const PORT = 3000;
+
+app.listen(PORT, function() {
+  console.log(`Server running on port: ${PORT}`);
+});
