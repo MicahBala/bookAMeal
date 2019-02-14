27afa58a2082f49e4b905b382db9c3c833be5da8
Micah Y. Bala <micahkulien@gmail.com>
2019-02-14 22:18:46 +0100

    [Feature #163844411] Added an endpoint to get all meals from a json file

--- app/api/app.js
+++ app/api/app.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import meals from './db/meals.db';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('The app is working fine');
+  res.send('Welcome to the meal booking app');
+});
+
+// get all meals
+app.get('/api/v1/meals', (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: 'meals retrieved successfully',
+    meals,
+  });
 });
 
 const PORT = 3000;
@@ -12,5 +26,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
 
-// module.exports = app;
 export default app;
---
+++ app/api/db/meals.db.js
@@ -0,0 +1,10 @@
+const meals = [
+  {
+    id: 1,
+    name: 'Bacon',
+    description: 'Bacon ipsum dolor amet ribeye.',
+    price: 'N400',
+  },
+];
+
+export default meals;
