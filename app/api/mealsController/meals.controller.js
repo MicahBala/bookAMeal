80e0fef3cadb5b54e26cfc02120f009bdea97228
Micah Y. Bala <micahkulien@gmail.com>
2019-02-16 11:08:46 +0100

    [Feature #163844436] Create api to add a meal option

--- app/api/mealsController/meals.controller.js
+++ app/api/mealsController/meals.controller.js
@@ -1,17 +1,42 @@
-import mealsDb from '../db/meals.db';
+import mealsDb from "../db/meals.db";
 
 class MealsController {
   // get home page
   getHomePage(req, res) {
-    return res.send('Welcome to the meal booking app');
+    return res.send("Welcome to the meal booking app");
   }
 
   //   get all meals
   getAllMeals(req, res) {
     res.status(200).send({
       success: true,
-      message: 'meals retrieved successfully',
-      meals: mealsDb,
+      message: "meals retrieved successfully",
+      meals: mealsDb
+    });
+  }
+
+  //   add a meal
+  addMeal(req, res) {
+    if (!req.body.name || !req.body.description || !req.body.price) {
+      return res.status(404).send({
+        success: false,
+        message: "name, description and price required"
+      });
+    }
+
+    const meal = {
+      id: mealsDb.length + 1,
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price
+    };
+
+    mealsDb.push(meal);
+
+    return res.status(200).send({
+      success: true,
+      message: "meal added successfully",
+      meals: mealsDb[mealsDb.length - 1]
     });
   }
 }
--- app/api/routes/meals.route.js
+++ app/api/routes/meals.route.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import mealsController from '../mealsController/meals.controller';
+import express from "express";
+import mealsController from "../mealsController/meals.controller";
 
 const router = express.Router();
 
-router.get('/', mealsController.getHomePage);
+router.get("/", mealsController.getHomePage);
 
 // get all meals
-router.get('/api/v1/meals', mealsController.getAllMeals);
+router.get("/api/v1/meals", mealsController.getAllMeals);
+
+// add a meal
+router.post("/api/v1/meals", mealsController.addMeal);
 
 export default router;
