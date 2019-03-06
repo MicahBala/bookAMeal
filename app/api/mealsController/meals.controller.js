24a88e0fe3e5555228a9a742effa82bd2768c8ff
Micah Y. Bala <micahkulien@gmail.com>
2019-03-06 10:17:44 +0100

    [Feature #163844560] Update a meal from the database

--- app/api/mealsController/meals.controller.js
+++ app/api/mealsController/meals.controller.js
@@ -62,36 +62,19 @@ class MealsController {
 
   // update meal info
   updateMeal(req, res) {
-    const mealToUpdate = mealsDb.find(
-      updMeal => updMeal.id === parseInt(req.params.id, 10)
-    );
-
-    if (!mealToUpdate) {
-      return res.status(404).send({
-        success: false,
-        message: "meal not found"
+    let { name, description, price } = req.body;
+    mealsDb
+      .update(
+        { name, description, price },
+        { returning: true, where: { id: req.params.id } }
+      )
+      .then(([rowsUpdated, [updatedMeal]]) => {
+        return res.status(200).send({
+          success: true,
+          message: "meal updated successfully",
+          updatedMeal
+        });
       });
-    }
-
-    const schema = {
-      name: Joi.string(),
-      description: Joi.string(),
-      price: Joi.string()
-    };
-
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-      return res.status(404).send("meal not found");
-    }
-
-    mealToUpdate.name = req.body.name || mealToUpdate.name;
-    mealToUpdate.description = req.body.description || mealToUpdate.description;
-    mealToUpdate.price = req.body.price || mealToUpdate.price;
-
-    return res.status(200).send({
-      success: true,
-      message: "meal updated successfully"
-    });
   }
 }
 
